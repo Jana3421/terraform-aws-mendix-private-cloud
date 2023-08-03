b5757e4a26417cd439fb41c0fcf01dbea7c6b5a3
Jana3421 <98213631+Jana3421@users.noreply.github.com>
2023-08-03 16:05:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "eks_blueprints" {
 }
 
 module "eks_blueprints_kubernetes_addons" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.27.0"
+  source = "git::ssh://git@github.com/aws-ia/terraform-aws-eks-blueprints.git//modules/kubernetes-addons/karpenter?ref=v4.32.1"
 
   eks_cluster_id       = module.eks_blueprints.eks_cluster_id
   eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
